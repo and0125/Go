2edd9599c03833789f039741f95401c43a92f097
Aaron Dantley <aarondantley@gmail.com>
2022-06-03 18:29:55 -0400

    Go Exercises Added the hello-world project; added the start of a quiz-game project

---
+++ hello-world/hello-world.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// This will output text to the command line
+
+func main() {
+	fmt.Println("Hello World!")
+}
---
+++ quiz-game/quiz-game.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Time to Play the Quiz Game!")
+
+	var name string = "Aaron" // How you create variables in Go
+
+	fmt.Println(name)
+}
